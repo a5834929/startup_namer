442c9fcb8a4c41caebc954f4ff6983e2120be17c
a5834929 <a5834929@gmail.com>
2018-12-18 11:17:46 -0800

    add inifite list of word pairs as users scroll

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,40 @@ class RandomWords extends StatefulWidget{
 }
 
 class RandomWordsState extends State<RandomWords>{
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
-  Widget build(BuildContext context){
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+  Widget _buildRow(WordPair pair){
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont
+        ),
+      );
+  }
+  @override
+  Widget _buildSuggestions(){
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i){
+        if(i.isOdd) return Divider();
+        final index = i~/2;
+        if(index >= _suggestions.length){
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+        ),
+      body: _buildSuggestions(),
+      );
   }
 }
 
@@ -20,15 +50,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Namer',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Startup Namer'),
-        ),
-        body: Center(
-          child: RandomWords(),
-        ),
-      ),
-    );
+      title: 'Startup Name Generator',
+      home: RandomWords(),
+      );
   }
 }
